3343e24b689049e3f7dca84d52bfdf11c887835e
[hanne-97] <[hmendoza1622@gmail.com]>
2024-04-19 15:03:43 -0600

    No me funciona el bubblesort :')

--- src/lab1p2_hannemendoza/Lab1P2_HanneMendoza.java
+++ src/lab1p2_hannemendoza/Lab1P2_HanneMendoza.java
@@ -28,13 +28,16 @@ public class Lab1P2_HanneMendoza {
             
             System.out.println("Matriz ordenada:"); //forma recursiva bubble sort(ascendente)
             int [] array = BubbleSort(mat);
+            imprimirArreglo(array);
             
             System.out.println("Arreglo de medianas: ");
-            
+            // para calcular las medianas mitad/2 + 1
+            // ir añadiendolas en un arraylist
+            //sout
             System.out.println("Arreglo de medianas ordenado: ");
-               
+            //bubble sort   
             System.out.println("Mediana de las medianas: ");
-            
+            //calcular mitad/2 +1
         }else{
             System.out.println("Número inválido debe ser mayor que 4 e impar");
         }
@@ -62,22 +65,28 @@ public class Lab1P2_HanneMendoza {
         }//fin método imprimirMatriz
     
     public static int [] BubbleSort(int [][] matriz){              
-        int [] temp = new int [matriz.length];
-        boolean cambiado = false;
-        int n;
-        int [] array;
+        int temp;
+        int pos = 0;
+        int [] array = new int [matriz.length];
         //caso base
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
-                if (array[j] > array[j + 1]) {
+        for (int i = 0; i < pos-1; i++){
+            for (int j = 0; j < pos-i-1; j++){
+               if (array[j] > array[j + 1]){
                     temp = array[j];
                     array[j] = array[j + 1];
-                    array[j + 1] = temp;
-                    cambiado = true;
-                }
-            }
+                    array[j + 1] = temp;                    
+                } 
+            }                
+        }
         //caso recursivo    
-        return 
-    } 
+        return array; 
+    }//fin método BubbleSort    
     
+     public static void imprimirArreglo(int[] array){        
+        for (int i = 0; i < array.length; i++) {
+            System.out.print("[" + array[i] + "] ");            
+        }
+        System.out.println("");
+        }//fin método imprimirArreglo
+        
 }//clase
